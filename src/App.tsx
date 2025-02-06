58c5c230b629a815726b61e4151310e264381c82
YoungMin Kwon <mmmyoung95@gmail.com>
2025-02-06 15:39:05 +0900

    Fix : 문구 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,43 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import MainPage from "./pages/MainPage";
+import MainPage from './pages/MainPage';
+import Task3Page from './pages/Task3Page';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen w-full">
-        <nav className="bg-gray-50 border-b border-gray-200 px-5 py-4">
-          <ul className="flex gap-5">
+      <div className='min-h-screen w-full'>
+        <nav className='bg-gray-50 border-b border-gray-200 px-5 py-4'>
+          <ul className='flex gap-5'>
             <li>
-              <a href="/" className="text-gray-700 hover:text-blue-600 font-medium">
-                Main
+              <a href='/' className='text-gray-700 hover:text-blue-600 font-medium'>
+                Home
               </a>
             </li>
             <li>
-              <a href="/task1" className="text-gray-700 hover:text-blue-600 font-medium">
-                Task 1 - Image Gallery
+              <a href='/task1' className='text-gray-700 hover:text-blue-600 font-medium'>
+                Task 1 - 이미지 갤러리
               </a>
             </li>
             <li>
-              <a href="/task2" className="text-gray-700 hover:text-blue-600 font-medium">
-                Task 2 - Card Game
+              <a href='/task2' className='text-gray-700 hover:text-blue-600 font-medium'>
+                Task 2 - 카드 게임
               </a>
             </li>
             <li>
-              <a href="/task3" className="text-gray-700 hover:text-blue-600 font-medium">
-                Task 3 - String Processing
+              <a href='/task3' className='text-gray-700 hover:text-blue-600 font-medium'>
+                Task 3 - 문자열 처리
               </a>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="/" element={<MainPage />} />
+          <Route path='/' element={<MainPage />} />
           {/* <Route path="/task1" element={<Task1Page />} />
-          <Route path="/task2" element={<Task2Page />} />
-          <Route path="/task3" element={<Task3Page />} /> */}
+          <Route path="/task2" element={<Task2Page />} /> */}
+          <Route path='/task3' element={<Task3Page />} />
         </Routes>
       </div>
     </BrowserRouter>
